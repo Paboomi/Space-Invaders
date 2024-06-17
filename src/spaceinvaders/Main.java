ea1b46c1b2cfbe25f45616c45ee68d3c83f50007
saien <saien@saien>
2024-06-16 22:20:16 -0600

    Menu Principal

---
+++ src/spaceinvaders/Main.java
@@ -0,0 +1,14 @@
+package spaceinvaders;
+
+import spaceinvaders.frontend.Principal;
+
+/**
+ *
+ * @author saien
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    new Principal();
+    }
+}
